c3174e75eb40f25256949d680495f384b0ec8cd2
gao90022308 <62718083+gao90022308@users.noreply.github.com>
2022-03-19 20:18:35 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="utf-8">
+  <title>Welcome!</title>
+</head>
+<body>
+  <h1 align="center" >Hello! You see my test page.</h1>
+</body>
+</html>
